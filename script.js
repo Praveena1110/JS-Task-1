1d57b429e0b896e52b6ee15d20a89a4b8c7d2733
Praveena1110 <praveenamurugesan002@gmail.com>
2026-06-24 12:48:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+// Task-1 Employee Name
+let employeeName=prompt("Enter Employee Name:");
+console.log("Employee Name:", employeeName);
+document.writeln("<h3>Welcome " + " " + employeeName + "</h3>");
+
+
+// Task-2 Company entry confirmation
+let trainingConfirm = confirm("Are you ready to join today's JavaScript training?");
+console.log("Training Confirmation:", trainingConfirm);
+
+
+// Task-3 Customer Greeting
+alert("Welcome to Stackly Solutions!");
+console.log("Customer entered the website.");
+
+
+// Task-4 Student Details
+let studentName = prompt("Enter Student Name:");
+let studentAge = prompt("Enter Student Age:");
+console.log("Student Name:", studentName);
+console.log("Student Age:", studentAge);
+
+
+// Task-5 Product Price
+let productPrice = 350;
+productPrice = 420;
+console.log("Update Product Price:", productPrice);
+
+
+// Task-6 Login Validation
+let username = prompt("Enter Username:");
+
+if (username === "" || username === null) {
+  console.warn("Warning: Username is empty.");
+} else {
+  console.log("Login Successful:", username);
+}
+
+
+// Task-7 Website Maintenance
+alert("Website is under maintenance");
+console.error("Error: Website is currently unavailable,");
+
+
+// Task-8 Feedback Collection
+let feedback = prompt("How was today's JavaScript session");
+console.log("Feedback:",feedback);
+document.writeln("<p>Thank you for your feedback!</p>");
+
+
+// Task-9 Profile information
+let name = prompt("Enter your name:");
+let city = prompt("Enter your city:");
+let favoriteLanguage = prompt("Enter your favorite programming language:");
+console.log("Name:", name);
+console.log("City:", city);
+console.log("Favorite programming language:", favoriteLanguage);
+
+
+// Task-10 Mini registration form
+let fullname = prompt("Enter your Full name:");
+let email = prompt("Enter your email id:");
+let mobile = prompt("Enter your mobile number:");
+console.log("=====Registration Details=====");
+console.log("Name:", fullname);
+console.log("Email:", email);
+console.log("Mobile:", mobile);
+alert("Register Successfully!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
